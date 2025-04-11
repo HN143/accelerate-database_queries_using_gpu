4d7f4d0413baf0748c6c12c9e4daff2cfc07ee65
To Quang Vien <vien10.02.2003@example.com>
2025-04-11 09:59:33 +0700

    update to ignore overRAM

--- src/heavydb/with_tpcds/duckdb_script/use_duckdb_generate_1gb_data.sh
+++ src/heavydb/with_tpcds/duckdb_script/use_duckdb_generate_1gb_data.sh
@@ -9,6 +9,7 @@ fi
 DATA_SIZE=$1  # Giá trị scale factor, ví dụ: 1, 5, 10, v.v.
 DUCKDB_BIN="duckdb"  # Có thể là ./duckdb nếu bạn dùng file local
 TARGET_DIR="exported_data"  # Thư mục lưu dữ liệu
+DATA_DIR="data.duckdb"  # Thư mục lưu dữ liệu
 
 # Xóa thư mục nếu đã tồn tại, sau đó tạo mới
 if [ -d "$TARGET_DIR" ]; then
@@ -19,12 +20,14 @@ fi
 mkdir -p "$TARGET_DIR"
 
 # Chạy DuckDB với scale factor được truyền vào
-./$DUCKDB_BIN <<EOF
+./$DUCKDB_BIN $DATA_DIR<<EOF
 INSTALL tpcds;
 LOAD tpcds;
 SELECT * FROM dsdgen(sf=${DATA_SIZE});
 EXPORT DATABASE '$TARGET_DIR' (FORMAT CSV, DELIMITER '|');
 EOF
+
+rm -f -v $DATA_DIR
 
 echo "DuckDB has completed execution with sf=${DATA_SIZE}. Data exported to: $TARGET_DIR"
 
